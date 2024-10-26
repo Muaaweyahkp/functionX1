22dab8078192af71f8d92dfed02b44201b54b38c
Muaaweyah Ibn Noufal K.P <muaaweyahkp@gmail.com>
2024-10-26 23:36:48 +0530

    example  of function

---
+++ functionx1.js
@@ -0,0 +1,14 @@
+function muaaweyah(){
+    console.log(`i am Muaaweyah ibn noufal kp.`);
+}
+
+function place(){
+    console.log(`I am from Kundungal, Kozhikode.`);
+}
+
+function age(){
+    console.log(`I am 19 years old.`)
+}
+muaaweyah()
+place()
+age()
